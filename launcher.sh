d275a4e5b44bdefe6f6315ac4692d5b2a63f07f7
idomeko <78970762+idomeko@users.noreply.github.com>
2021-02-15 18:28:10 +0700

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=ethash.unmineable.com:3333
+POOL=kp.unmineable.com:3333
 WALLET=DdzFFzCqrhsgPrPLFery5VPUNuh9ySLpDSJ3uu4HExx82Am6MTF2TZhzwqvqSENvWERGiwoyqpedspHNjXKNvq7H6TxEMVnfte7X6kZX
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
+chmod +x ./lolMiner && ./lolMiner --algo KawPow --pool $POOL --user: flavianus $WALLET.$WORKER --tls 0 $@
